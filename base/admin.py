637f4781a51f39383e2b870f7c506705904009ce
Andres Vargas <andres.vargas@interalia.net>
2016-11-17 09:07:06 -0600

    fix the opneload file

--- base/admin.py
+++ base/admin.py
@@ -6,7 +6,10 @@ from .models import *
 admin.site.register(Media)
 
 class EntryAdmin(admin.ModelAdmin):
-    list_display = ("__str__", "media", "episode", "season")
+    list_display = ("__str__", "media", "episode", "season","get_resources")
+
+    def get_resources(self, obj):
+        return obj.resources.count()
 
 admin.site.register(Entry, EntryAdmin)
 
